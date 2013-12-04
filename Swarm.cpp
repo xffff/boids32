4fc2890c360774e5ca93a31f54bab3da17bd5a35
unknown <z0039k7t@MD177F7C.ww002.siemens.net>
2013-12-04 15:59:18 +0000

    forgot to commit something I hope it is not incorrect

--- Swarm.cpp
+++ Swarm.cpp
@@ -10,12 +10,14 @@ Swarm::Swarm(int numAgents)
 {
     srand(time(NULL)); // seed random gen
     std::cout<<"Swarm creating " <<numAgents<< " Agents...\n";
+    for(int i=0; i<numAgents; i++) { addAgent(); }
 }
 
 void Swarm::addAgent()
 {
     float vals[4];
-    for(int i=0; i<4; i++) vals[i] = (float)(((rand() % RAND_MAX) / RAND_MAX) * 2) - 1;
-    std::unique_ptr<Agent> temp(new Agent());
+    for(int i=0; i<4; i++) { vals[i] = (float)(((rand() % RAND_MAX) / RAND_MAX) * 2) - 1; }
+    std::unique_ptr<Agent> temp(new Agent(vals[0], vals[1], vals[2], vals[3]));
+    std::cout<<i<<": "<<vals[0]<<", "<<vals[1]", "<<vals[2]<<", "<<vals[3]<<std::endl;
     agents.push_back(std::move(temp));
 }
