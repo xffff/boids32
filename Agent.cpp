e98677d183c3812f238de4244c392677f85b508f
unknown <z0039k7t@MD177F7C.ww002.siemens.net>
2013-12-04 15:50:34 +0000

    easier to just get rid of agent files completely they are annoying and useless

--- Agent.cpp
+++
@@ -1,9 +0,0 @@
-#include "Agent.h"
-
-Agent::Agent(float x, float y, float vx, float vy)
-{
-    this->x = x;
-    this->y = y;
-    this->vx = vx;
-    this->vy = vy;
-}
--- Swarm.cpp
+++ Swarm.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <memory>
 #include <stdio.h>
@@ -7,5 +8,14 @@
 
 Swarm::Swarm(int numAgents)
 {
+    srand(time(NULL)); // seed random gen
+    std::cout<<"Swarm creating " <<numAgents<< " Agents...\n";
+}
 
+void Swarm::addAgent()
+{
+    float vals[4];
+    for(int i=0; i<4; i++) vals[i] = (float)(((rand() % RAND_MAX) / RAND_MAX) * 2) - 1;
+    std::unique_ptr<Agent> temp(new Agent());
+    agents.push_back(std::move(temp));
 }
