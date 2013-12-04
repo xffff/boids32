baa654a279860d46320484fc90af968a3c800367
unknown <z0039k7t@MD177F7C.ww002.siemens.net>
2013-12-04 10:15:10 +0000

    start making agent, no need to implement behaviour class - every swarm has these attributes anyway

---
+++ Agent.cpp
@@ -0,0 +1,14 @@
+#include "Agent.h"
+
+Agent::Agent(float x, float y, float vx, float vy)
+{
+    self->x = x;
+    self->y = y;
+    self->vx = vx;
+    self->vy = vy;
+}
+
+Agent::~Agent()
+{
+    
+}
