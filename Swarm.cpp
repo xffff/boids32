b043594e99b9229f18d90a984f23c7d52fed1a43
unknown <z0039k7t@MD177F7C.ww002.siemens.net>
2013-12-05 09:51:16 +0000

    compiles like this, but does it work

--- Swarm.cpp
+++ Swarm.cpp
@@ -6,7 +6,7 @@
 #include <time.h>
 #include "Swarm.h"
 
-Swarm::Swarm(int numAgents)
+Swarm::Swarm(int numAgents) : numAgents(numAgents)
 {
     srand(time(NULL)); // seed random gen
     std::cout<<"Swarm creating " <<numAgents<< " Agents...\n";
@@ -26,4 +26,32 @@ void Swarm::addAgent()
 	     <<vals[3]<<std::endl;
     std::unique_ptr<Agent> p(new Agent(vals[0], vals[1], vals[2], vals[3]));
     agents.push_back(std::move(p));    
+}
+
+std::vector<std::vector<float> > Swarm::getBoids()
+{
+    float cx, cy, cvx, cvy;
+    std::vector<std::vector<float> > swarmVals; // return this
+    
+    for(int i=0; i<numAgents; i++) {
+	std::vector<float> agentVals;
+	genFrame(i);
+	cx += agents[i]->x;
+	cy += agents[i]->y;
+	cvx += agents[i]->vx;
+	cvy += agents[i]->vy;
+	agentVals.push_back(agents[i]->x);
+	agentVals.push_back(agents[i]->y);
+	agentVals.push_back(agents[i]->vx);
+	agentVals.push_back(agents[i]->vy);
+	swarmVals.push_back(agentVals);
+    }
+    centroid[0] = cx/numAgents;
+    centroid[1] = cy/numAgents;
+    avgvelocity[0] = cvx/numAgents;
+    avgvelocity[1] = cvy/numAgents;
+}
+
+void Swarm::genFrame(int agentIndex) {
+    ;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include "Swarm.h"
 
-int main()
+typedef std::vector< std::vector<float> > vvfloat;
+
+int main(int argc, char** argv)
 {
     std::cout<<"win32 boids v20131204\n";
     Swarm s = Swarm(10);
+    vvfloat v;
+    v = s.getBoids();
+    for(int i=0; i<s.numAgents; i++) {
+	std::vector<float> agentData = v[i];
+	std::cout<<"Agent "<<i<<": ";
+	for(std::vector<float>::iterator it = agentData.begin(); it != agentData.end(); ++it)
+	    { std::cout<<*it<<", "; }
+	std::cout<<std::endl;
+    }
     return 0;
 }
 
